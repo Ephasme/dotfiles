7b251659d42d453d5840ca544fcd48d0c8479514
Ephasme <loup.peluso@gmail.com>
2022-07-05 07:00:11 +0200

    wip

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/sh
 
-sudo apt-get install -y snapd tmux python3 zoxide
+sudo apt-get install -y snapd tmux python3 zoxide zsh
+sudo apt-get install kubectl
+sudo chsh loup --shell /bin/zsh
 sudo snap install code --classic
 sudo snap install starship
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
+
+fc-cache -fv
+
+npm install -g pnpm
