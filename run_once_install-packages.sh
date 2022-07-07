2587e61ace96f2648310bcdfb42a8cf5f9daac3c
Ephasme <loup.peluso@gmail.com>
2022-07-07 23:26:10 +0200

    wip

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,7 +5,13 @@ sudo apt-get install kubectl gh
 sudo chsh loup --shell /bin/zsh
 sudo snap install code --classic
 sudo snap install starship
+sudo snap install docker
 sudo snap install slack --classic
+
+sudo add-apt-repository -y ppa:hluk/copyq
+sudo apt-get update -y
+sudo apt-get install copyq -y
+
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
 fc-cache -fv
